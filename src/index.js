e4c30237a4a1948f160ae20d616ab96b3524593d
ulfetalhussaini <ulfetalhussaini@yahoo.com>
2021-09-01 00:01:26 +0300

    update

--- src/index.js
+++ src/index.js
@@ -4,9 +4,10 @@ import App from "./App";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
 import reducer from "./features/books/booksSlice";
+import rootReducer from "./reducer";
 
 const store = createStore(
-  reducer,
+  rootReducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
---
+++ src/reducer.js
@@ -0,0 +1,11 @@
+  
+import { combineReducers } from "redux";
+import authorsReducer from "./features/authors/authorsSlice";
+import booksReducer from "./features/books/booksSlice";
+
+const rootReducer = combineReducers({
+  authors: authorsReducer,
+  books: booksReducer,
+});
+
+export default rootReducer;
